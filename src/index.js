00716fcb1bb61795dff5a39ee8141c9f2493d910
Yupinnu <83113114+Yupinnu@users.noreply.github.com>
2021-05-19 20:51:42 +0700

    finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Yupin";
+const name2 = "Yupenis";
+const num = 69;
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {name} {name2}!
+    </h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
